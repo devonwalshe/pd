00f6a2de73afc8dbd01a887fd1c2aef064bd029a
sheinin <sergei.sheinin@gmail.com>
2020-07-29 17:32:57 +0700

    React UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import PD from './PD/PD.js'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./DownshiftCheckbox/downshift-checkbox.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './bootstrap2-toggle.css'
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 //const restURL = 'http://0.0.0.0:8000/pipe_section.json'//
 const restURL = 'http://localhost:5000/'
--- frontend/src/PD/PD.js
+++ frontend/src/PD/PD.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import { Form, Dropdown, Toast } from 'react-bootstrap';
-import Popup from "reactjs-popup";
-import DownshiftCheckbox from "../DownshiftCheckbox/DownshiftCheckbox";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Form, Dropdown, Toast } from 'react-bootstrap'
+import { Typeahead } from 'react-bootstrap-typeahead'
+import Feature from './Feature.js'
 import fontawesome from '@fortawesome/fontawesome'
-import { faSpinner, faSearchPlus, faSearchMinus } from '@fortawesome/free-solid-svg-icons'
+import { faCrosshairs, faFilter, faSpinner, faSearchPlus, faSearchMinus } from '@fortawesome/free-solid-svg-icons'
+import Toggle from 'react-bootstrap-toggle'
 
-fontawesome.library.add(faSpinner, faSearchPlus, faSearchMinus);
+fontawesome.library.add(faCrosshairs, faFilter, faSpinner, faSearchPlus, faSearchMinus);
 
 export default class PD extends Component {
 
@@ -16,12 +17,16 @@ export default class PD extends Component {
 
         this.state = {
 
-            features_add: [],
             features_filter: [],
+            match_on: false,
+            filter: {
+                matched:true,
+                unmatched: true
+            },
             is_loading: false,
             rest_error: false,
             pipe_section: [],
-            pipe_sections: [],
+            pipe_section_select: [],
             pipe_section_instance: 0,
             run_matches: [],
             run_match_instance: 0
@@ -37,7 +42,7 @@ export default class PD extends Component {
     componentDidMount() {
 
         this.getGraphWidth()
-        this.setPipeFeatures()
+        this.setPipeSections()
         this.fetchRest('run_matches', null, (data) => {
 
             this.setRunMatches(data, null, this.setRunMatches)
@@ -62,7 +67,6 @@ export default class PD extends Component {
                     cbk(data, instance)
                 })
                 .catch(e => {
-                    console.log(e)
                     this.setState({is_loading:false})
                     this.setState({rest_error: true})
                 })
@@ -100,12 +104,15 @@ export default class PD extends Component {
 
     selectPipeSection = e => {
 
-        const i = Number(e.currentTarget.options[e.currentTarget.selectedIndex].value)
+        if (!e || !e[0] || !e[0].key)
+
+            return
+
+        const i = Number(e[0].key)
         
         this.fetchRest('pipe_section', i, data => {
             
             this.pipe_section_raw = data
-            this.setPipeFeatures()
             this.displayPipeSection()
             
         })
@@ -113,72 +120,6 @@ export default class PD extends Component {
 
     }
 
-    setPipeFeatures = data => {
-
-        const pipe = this.pipe_section_raw.features || []
-
-        let features = [],
-            out = []
-
-        for (let i = 0, ix = pipe.length; i < ix; i += 1)
-
-            for (let j = 0, jx = pipe[i].attributes.length; j < jx; j += 1) {
-
-                const { attribute_data, attribute_name } = pipe[i].attributes[j]
-
-                if (attribute_name === 'feature_category' && !~features.indexOf(attribute_data)) {
-
-                    features.push(attribute_data)
-                    out.push({label: attribute_data, value: attribute_data})
-
-                }
-            }
-        
-        const features_add = out.map((item, index) => <Dropdown.Item key={index} onClick={()=>alert(item.value)}>{item.label}</Dropdown.Item>)
-        const features_filter = <DownshiftCheckbox key="feature_filter" itemToString={item => (item ? item.label : "")}>
-            {({
-                getToggleButtonProps,
-                selectedItems,
-                getItemProps,
-                isOpen,
-                actionType
-            }) => (
-                <div className="custom-select-checkbox">
-                <button {...getToggleButtonProps()} className="custom-select-button">
-                    Features Filter
-                </button>
-                {!isOpen ? null : (
-                    <div className={`item-list ${isOpen ? "open" : ""} `}>
-                        {out.map((item, index) => (
-                            <label
-                                className="checkbox-label custom-select-label"
-                                htmlFor={item.value}
-                                key={item.value}
-                            >
-                            <input
-                                type="checkbox"
-                                {...getItemProps({
-                                    item,
-                                    index,
-                                    checked: selectedItems.includes(item)
-                                })}
-                                id={item.value}
-                                value={item.value}
-                                onChange={() => null}
-                            />
-                            <span />
-                            {item.label}
-                            </label>
-                        ))}
-                    </div>
-                )}
-                </div>
-            )}
-        </DownshiftCheckbox>
-    
-        this.setState({features_add: features_add, features_filter: [features_filter]})
-
-    }
 
     setPipeSections = data => {
 
@@ -186,19 +127,23 @@ export default class PD extends Component {
         
         let options = []
 
-        for (let i = 0, ix = data.length; i < ix; i += 1)
+        for (let i = 0, ix = (data || []).length; i < ix; i += 1)
 
-            if (run_match === data[i].run_match)
-            
-                options.push(<option key={i} value={data[i].id}>{data[i].section_id}</option>)
+            if (run_match === data[i].run_match) //## this line will need to remove once RESTful supports run_match filter
+          
+                options.push({key: i, label: data[i].section_id})
 
-        this.setState({pipe_sections: options})
+        const select = <Typeahead
+                        id="basic-typeahead-single"
+                        onChange={this.selectPipeSection}
+                        options={options}
+                        placeholder="Pipe Section..." />
+
+        this.setState({pipe_section_select: select})
 
     }
 
     displayPipeSection = () => {
-
-        const supported = ['flange','valve']
 
         let features = [],
             w = 0,
@@ -208,11 +153,15 @@ export default class PD extends Component {
         const pipe = this.pipe_section_raw.features || []
 
         for (let i = 0, ix = pipe.length; i < ix; i += 1) {
-console.log(ix)
+
             let feature = {
 
                     attributes: {},
-                    id: pipe[i].id
+                    id: pipe[i].id,
+                    side: pipe[i].side,
+                    matched: pipe[i].matched,
+                    left: 0,
+                    top: 0
 
                 }
 
@@ -226,46 +175,36 @@ console.log(ix)
                 
                     feature.left = w += Number(attribute_data)
 
+                else if (attribute_name === 'orientation_deg')
+
+                    feature.top = Number(attribute_data)
+
             }
             
-            feature.attributes.feature_category && out.push(feature)
+            feature.attributes.feature_category && ((this.state.filter.matched && feature.matched) || this.state.filter.unmatched && !feature.matched) && out.push(feature)
 
         }
-        console.log(out,w)
-        for (let i = 0, ix = out.length; i < ix; i +=1){
-            console.log(width / w * out[i].left + 'px')
-            features.push(<Popup
-                key={out[i].id + 'popup'}
-                trigger={
-                    <div
-                        className={"shape " + (!~supported.indexOf(out[i].attributes.feature_category) ? 'unknown' : out[i].attributes.feature_category)}
-                        key={out[i].id}
-                        style={{
-                            left: width / w * out[i].left + 'px'
-                        }}>
-                    </div>
-                }
-                keepTooltipInside="#pipeline_graph_container"
-                on="hover"
-            >
-                <div className="card">
-                    <div className="content">
-                        {(data => {
-                            let out = []
-                            for (let attr in data) {
-                                out.push (<b key={attr + 'b'}>{attr}</b>)
-                                out.push (<span key={attr + 'c'}>:</span>)
-                                out.push (<span key={attr + 'd'}>{data[attr]}</span>)
-                                out.push (<br key={attr + 'e'} />)
-                            }
-                            return out
-                        })(out[i].attributes)}
-                    </div>
-                </div>                
-            </Popup>)
+
+        for (let i = 0, ix = out.length; i < ix; i +=1) {
+
+            out[i].left = width / w * out[i].left
+            features.push(<Feature key={'feature_' + i} feature={out[i]} />)
+
         }
-        console.log(features)
+        
         this.setState({pipe_section: features})
+
+    }
+    
+    setMatchFilter = e => {
+
+        const val = e.target.value
+        const filter = {
+            matched: val === 'matched' ? !this.state.filter.matched : this.state.filter.matched,
+            unmatched: val === 'unmatched' ? !this.state.filter.unmatched : this.state.filter.unmatched,
+        }
+
+        this.setState({filter: filter}, this.displayPipeSection)
 
     }
 
@@ -277,33 +216,45 @@ console.log(ix)
             <div>
                 <div style={{backgroundColor:'#eee', display:'inline-block', padding:10, width:'100%'}}>
                     <div style={{alignItems:'baseline',display:'flex',float:'left'}}>
+                    
                         <Form.Label style={{marginRight:'10px'}}>Run:</Form.Label>
                         <Form.Control as="select" onChange={this.selectRunMatch} style={{width:'200px'}}>
                             <option>Run Matches...</option>
                             {this.state.run_matches}
                         </Form.Control>
                         <Form.Label style={{marginLeft:'20px',marginRight:'10px'}}>Pipe:</Form.Label>
-                        <Form.Control as="select" onChange={this.selectPipeSection} style={{width:'200px'}}>
-                            <option>Pipe Sections...</option>
-                            {this.state.pipe_sections}
-                        </Form.Control>
+
+                        {this.state.pipe_section_select}
+
                     </div>
                     {this.state.is_loading && (<div style={{float:'right'}}>
                         <i className="fa fa-spinner fa-spin" />
                     </div>)}
                 </div>
                 <div style={{backgroundColor:'#fff', display:'inline-block', padding:10, width:'100%'}}>
-                    <div style={{display:"flex", float:"left"}}>
-                        {this.state.features_filter}
+                    <div style={{alignItems:'center', display:"flex", float:"left"}}>
+                        <div style={{backgroundColor:'#DDD',marginRight:'10px',  paddingBottom:5, paddingLeft: 10, paddingRight: 10, paddingTop: 5}} title="Feature Filter">
+                            <i className="fa fa-filter"></i>
+                        </div>
+                        <Form.Check type="checkbox" value="matched" label="Matched" onChange={this.setMatchFilter} checked={this.state.filter.matched} />
                         <div style={{width:'15px'}}></div>
-                        <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                Add Feature
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                {this.state.features_add}
-                            </Dropdown.Menu>
-                        </Dropdown>
+                        <Form.Check type="checkbox" value="unmatched" label="Unmatched" onChange={this.setMatchFilter} checked={this.state.filter.unmatched} />
+                        <div style={{borderRight:'1px solid #444',marginLeft: 20, marginRight: 25, height: 30}}></div>
+                        <div style={{backgroundColor:'#DDD',marginRight:'10px', paddingBottom:5, paddingLeft: 10, paddingRight: 10, paddingTop: 5}} title="Feature Matching ON/OFF">
+                            <i className="fa fa-crosshairs"></i>
+                        </div>
+                        <Toggle
+                            active={this.state.match_on}
+                            id='match_toggle'
+                            on='ON'
+                            off='OFF'
+                            onstyle='primary'
+                            offstyle='default'
+                            width={68}
+                            height={30}
+                            onClick={() => this.setState({match_on: !this.state.match_on})}
+                        />
+
                     </div>
                     <div style={{display:'flex', direction:'row', alignItems:"center", float:"right"}}>
                         <i className="fa fa-search-minus" style={{marginRight:'10px'}}></i>
@@ -322,11 +273,12 @@ console.log(ix)
                     </Toast>
                     </div>
                     <div style={{height: '100%',width: '100%',padding:10}}>
-                        <div id="pipe_section_graph" style={{height: '100%',width: '100%', backgroundColor: '#ddd'}}>
+                        <div id="pipe_section_graph" style={{height: '100%',width: '100%', position:'relative', backgroundColor: '#ddd'}}>
                             {this.state.pipe_section}
                         </div>
                     </div>
                 </div>
+
             </div>
             
         )
